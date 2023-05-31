3761ff84645aaaacd247a10911322733507b70a7
REIGEN <daniil-yak@yandex.ru>
2023-06-01 01:25:04 +0300

    feat: beautiful navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PageOne, PageTwo, PageZero } from "./components/Pages";
+import { MainPage } from "./MainPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route index element={<PageZero />} />
+          <Route path="one" element={<PageOne />} />
+          <Route path="two" element={<PageTwo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Pages.jsx
@@ -0,0 +1,24 @@
+import { loremIpsum } from "lorem-ipsum";
+import styled from "styled-components";
+
+const StyledText = styled.p`
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  margin: 0px 10px 10px 10px;
+  padding: 10px;
+  font-size: ${(props) => props.$size || "14px"};
+`;
+
+const BuildPage = (index) => (
+  <>
+    <StyledText $size="20px">Page {index}</StyledText>
+    <StyledText>
+      Page {index} content: {loremIpsum({ count: 5 })}
+    </StyledText>
+  </>
+);
+
+export const PageZero = () => (
+  <StyledText>This is the main page, choose any other!</StyledText>
+);
+export const PageOne = () => BuildPage(1);
+export const PageTwo = () => BuildPage(2);

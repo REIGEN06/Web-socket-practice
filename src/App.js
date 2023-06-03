9f43c11d09d7b490c921d9d17dd2449b324b18a0
REIGEN <daniil-yak@yandex.ru>
2023-06-03 13:23:09 +0300

    Revert "Updates"
    This reverts commit d2e39c338e0daa10629174b2296b599f16ec2611.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PageOne, PageTwo, PageZero } from "./components/Pages";
+import { MainPage } from "./components/MainPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route index element={<PageZero />} />
+          <Route path="one" element={<PageOne />} />
+          <Route path="two" element={<PageTwo />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

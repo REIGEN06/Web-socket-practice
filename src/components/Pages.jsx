05b958c39f0c6ebfcd21493d5b0f6fb936c6d49d
REIGEN <daniil-yak@yandex.ru>
2023-06-06 01:55:57 +0300

    feat: primitive auth

--- src/components/Pages.jsx
+++ src/components/Pages.jsx
@@ -1,4 +1,5 @@
 import { loremIpsum } from "lorem-ipsum";
+import { useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 const StyledText = styled.p`
@@ -75,41 +76,133 @@ const BuildPage = (index) => {
     </Wrapper>
   );
 };
+
 const SignUpPage = () => {
+  const [state, setState] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!state.username) {
+      alert("Enter the name!");
+      return;
+    } else if (!state.email) {
+      alert("Enter the email!");
+      return;
+    } else if (!state.pass) {
+      alert("Enter the password!");
+      return;
+    } else if (!state.dblpass) {
+      alert("Confirm the password!");
+      return;
+    }
+    if (!(state.pass === state.dblpass)) {
+      alert("Password mismatch");
+      return;
+    }
+    const EMAIL_REGEXP =
+      /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    if (!state.email.match(EMAIL_REGEXP)) {
+      alert("Invalid email");
+      return;
+    }
+    localStorage.setItem("username", state.username);
+    localStorage.setItem("email", state.email);
+    localStorage.setItem("pass", state.pass);
+    localStorage.setItem("dblpass", state.dblpass);
+    window.location.reload();
+    alert("Account created!");
+  };
   return (
     <Wrapper>
-      <ColumnCenter>
-        <ColorChangeText>
-          <StyledText $size="20px">
-            Welcome to the Registration page!
-          </StyledText>
-        </ColorChangeText>
-        <StyledText>Username</StyledText>
-        <Input placeholder="Enter your Username" />
-        <StyledText>Email</StyledText>
-        <Input placeholder="Enter your Email" />
-        <StyledText>Password</StyledText>
-        <Input placeholder="Enter your password" $color="#f3bf0e" />
-        <StyledText>Repeat Password</StyledText>
-        <Input placeholder="Repeat your password" $color="#f3bf0e" />
-        <Submit $value="Create new account" />
-      </ColumnCenter>
+      <form onSubmit={handleSubmit}>
+        <ColumnCenter>
+          <ColorChangeText>
+            <StyledText $size="20px">
+              Welcome to the Registration page!
+            </StyledText>
+          </ColorChangeText>
+          <StyledText>Username</StyledText>
+          <Input
+            placeholder="Enter your Username"
+            onChange={(e) => {
+              setState({ ...state, username: e.target.value });
+            }}
+          />
+          <StyledText>Email</StyledText>
+          <Input
+            placeholder="Enter your Email"
+            onChange={(e) => {
+              setState({ ...state, email: e.target.value });
+            }}
+          />
+          <StyledText>Password</StyledText>
+          <Input
+            placeholder="Enter your password"
+            $color="#f3bf0e"
+            onChange={(e) => {
+              setState({ ...state, pass: e.target.value });
+            }}
+          />
+          <StyledText>Repeat Password</StyledText>
+          <Input
+            placeholder="Repeat your password"
+            $color="#f3bf0e"
+            onChange={(e) => {
+              setState({ ...state, dblpass: e.target.value });
+            }}
+          />
+          <Submit $value="Create new account" />
+        </ColumnCenter>
+      </form>
     </Wrapper>
   );
 };
 const LogInPage = () => {
+  const [state, setState] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(state);
+    if (!state.confirmEmail) {
+      alert("Enter the email!");
+      return;
+    } else if (!state.confirmPass) {
+      alert("Enter the password!");
+      return;
+    }
+    if (
+      localStorage.getItem("email") !== state.confirmEmail ||
+      localStorage.getItem("pass") !== state.confirmPass
+    ) {
+      alert("Invalid email or password!");
+      return;
+    }
+    window.location.reload();
+    alert("You are logged in!");
+  };
   return (
     <Wrapper>
-      <ColumnCenter>
-        <ColorChangeText>
-          <StyledText $size="20px">Welcome to the Log In page!</StyledText>
-        </ColorChangeText>
-        <StyledText>Email</StyledText>
-        <Input placeholder="Enter your Email" />
-        <StyledText>Password</StyledText>
-        <Input placeholder="Enter your password" $color="#f3bf0e" />
-        <Submit $value="Log in" />
-      </ColumnCenter>
+      <form onSubmit={handleSubmit}>
+        <ColumnCenter>
+          <ColorChangeText>
+            <StyledText $size="20px">Welcome to the Log In page!</StyledText>
+          </ColorChangeText>
+          <StyledText>Email</StyledText>
+          <Input
+            placeholder="Enter your Email"
+            onChange={(e) => {
+              setState({ ...state, confirmEmail: e.target.value });
+            }}
+          />
+          <StyledText>Password</StyledText>
+          <Input
+            placeholder="Enter your password"
+            $color="#f3bf0e"
+            onChange={(e) => {
+              setState({ ...state, confirmPass: e.target.value });
+            }}
+          />
+          <Submit $value="Log in" />
+        </ColumnCenter>
+      </form>
     </Wrapper>
   );
 };
